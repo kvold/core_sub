9975ff7d12454ab45346e00b5ef9841a4c2408d1
jarle pedersen <jarle.pedersen@norconsult.com>
2017-11-14 14:04:42 +0100

    test sending soap-call via post

--- CORESubscriber/Program.cs
+++ CORESubscriber/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
 
 namespace CORESubscriber
 {
@@ -6,7 +9,28 @@ namespace CORESubscriber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var getCapabilities = File.OpenText("Queries\\GetCapabilities.xml").ReadToEnd();
+            TestServiceCall(getCapabilities, args);
+        }
+
+        private static void TestServiceCall(string getCapabilities, string[] args)
+        {
+            var password = args[0];
+            var user = args[1];
+            var byteArray = Encoding.ASCII.GetBytes(user + ":" + password);
+            const string uri = "https://geosync.kystverket.no/kystverket/";
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+            var response = client.PostAsync(uri, new StringContent(getCapabilities, Encoding.UTF8, "text/xml"));
+
+            while (!response.IsCompleted && !response.IsCanceled && !response.IsFaulted)
+            {
+
+            }
+            Console.WriteLine(response.Result);
+
         }
     }
 }
