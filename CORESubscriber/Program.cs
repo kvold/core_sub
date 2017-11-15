75d5aa59603755608a7d341d1a81460519167e47
jarle pedersen <jarle.pedersen@norconsult.com>
2017-11-15 18:27:32 +0100

    removed unused code

--- CORESubscriber/Program.cs
+++ CORESubscriber/Program.cs
@@ -1,81 +1,35 @@
 ﻿using System;
 using System.IO;
-using System.Net;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace CORESubscriber
 {
-    class Program
+    internal class Program
     {
         private const string ApiUrl = "http://localhost:43397/WebFeatureServiceReplication.svc";
-        private static string myns = "http://skjema.geonorge.no/standard/geosynkronisering/1.1/produkt";
+        private static XNamespace Myns = "http://skjema.geonorge.no/standard/geosynkronisering/1.1/produkt";
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var getCapabilities = File.OpenText("Queries\\GetCapabilities.xml").ReadToEnd();
-            TestServiceCall(getCapabilities);
-            //TestServiceCall2(getCapabilities);
+            TestServiceCall();
         }
 
-        private static void TestServiceCall2(string getCapabilities)
-        {
-            try
-            {
-                using (var client = new HttpClient(new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip }) { Timeout = new TimeSpan(6000) })
-                {
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/xml"));
-                    //AddAuthorization(client);
-
-                    var response = client.SendAsync(CreateRequest(getCapabilities)).Result;
-
-                    if (!response.IsSuccessStatusCode)
-                    {
-                        throw new Exception();
-                    }
-
-                    Task<Stream> streamTask = response.Content.ReadAsStreamAsync();
-                    Stream stream = streamTask.Result;
-                    var sr = new StreamReader(stream);
-                    var soapResponse = XDocument.Load(sr);
-                    Console.WriteLine(soapResponse);
-
-                    //var xml = soapResponse.Descendants(myns + "GetCapabilities").FirstOrDefault().ToString();
-                    //var purchaseOrderResult = StaticMethods.Deserialize<GetStuffResult>(xml);
-                }
-            }
-            catch (AggregateException ex)
-            {
-                if (ex.InnerException is TaskCanceledException)
-                {
-                    throw ex.InnerException;
-                }
-                else
-                {
-                    throw ex;
-                }
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        private static HttpRequestMessage CreateRequest(string getCapabilities)
+        private static HttpRequestMessage CreateRequest(string soapXml, string action)
         {
             var request = new HttpRequestMessage
             {
                 RequestUri = new Uri(ApiUrl),
                 Method = HttpMethod.Post,
-                Content = new StringContent(getCapabilities, Encoding.UTF8, "text/xml")
+                Content = new StringContent(soapXml, Encoding.UTF8, "text/xml")
             };
 
             request.Headers.Clear();
             request.Content.Headers.ContentType = new MediaTypeHeaderValue("text/xml");
-            request.Headers.Add("SOAPAction", "http://skjema.geonorge.no/standard/geosynkronisering/1.1/produkt/#GetCapabilities");
+            request.Headers.Add("SOAPAction", Myns.NamespaceName + "/#" + action);
             return request;
         }
 
@@ -91,36 +45,31 @@ namespace CORESubscriber
 
         }
 
-        //public static T Deserialize<T>(string xmlStr)
-        //{
-        //    var serializer = new XmlSerializer(typeof(T));
-        //    T result;
-        //    using (TextReader reader = new StringReader(xmlStr))
-        //    {
-        //        result = (T)serializer.Deserialize(reader);
-        //    }
-        //    return result;
-        //}
-
-        private static void TestServiceCall(string getCapabilities)
+        private static void TestServiceCall()
         {
             var client = new HttpClient();
+
             //AddAuthorization(client);
-            
+
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/xml"));
 
-            var request = CreateRequest(getCapabilities);
+            var getCapabilities = File.OpenText("Queries\\GetCapabilities.xml").ReadToEnd();
+
+            var request = CreateRequest(getCapabilities, "GetCapabilities");
 
             var response = client.SendAsync(request);
 
-            while (!response.IsCompleted && !response.IsCanceled && !response.IsFaulted)
-            {
-
-            }
-
             var result = response.Result.Content.ReadAsStringAsync();
-            Console.WriteLine(result.Result);
 
+            var soap = XDocument.Parse(result.Result);
+
+            foreach (var dataset in soap.Descendants(Myns + "datasets").First().Descendants())
+            {
+                foreach (var field in dataset.Descendants())
+                {
+                    Console.WriteLine(field.Name.LocalName + ":" + field.Value);
+                }
+            }
         }
     }
 }
